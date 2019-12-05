6b6244a334c6d651d5ad3a3c744e89b81dba6379
Lordsun-eng <55994739+Lordsun-eng@users.noreply.github.com>
2019-12-05 16:38:30 +0300

    Splash

--- UI_REV0.py
+++ UI_REV0.py
@@ -12,6 +12,29 @@ from stl import mesh
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot
 
+## Splash Screen
+# create a splash screen, 80% of display screen size, centered,
+# displaying a GIF image with needed info, disappearing after 5 seconds
+root = tk.Tk()
+# show no frame
+root.overrideredirect(True)
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+root.geometry('%dx%d+%d+%d' % (width*0.8, height*0.8, width*0.1, height*0.1))
+# take a .jpg picture you like, add text with a program like PhotoFiltre
+# (free from http://www.photofiltre.com) and save as a .gif image file
+
+#assert os.path.exists(image_file)
+# use Tkinter's PhotoImage for .gif files
+image = tk.PhotoImage(file="Splash.gif")
+canvas = tk.Canvas(root, height=height*0.8, width=width*0.8, bg="brown")
+canvas.create_image(width*0.8/2, height*0.8/2, image=image)
+canvas.pack()
+# show the splash screen for 5000 milliseconds then destroy
+root.after(5000, root.destroy)
+root.mainloop()
+# your console program can start here ...
+print ("How did you like my informative splash screen?")
 
 def Opn():
     window.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.stl"),("all files","*.*")))
@@ -72,7 +95,7 @@ def Cyl():
 
 window = tk.Tk()
 ToolboxCreat = tk.Frame(window)
-
+window.iconbitmap('RaspSNK.ico')
 btnRec = Button(ToolboxCreat, text="Rectancle", width=10, command=Rec)
 btnRec.grid(column=0, row=1)
 btnSph = Button(ToolboxCreat, text="Sphere", width=10, command=Sph)
