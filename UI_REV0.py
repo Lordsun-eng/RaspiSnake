cbfbc69c07dcbed0edfeb94ad0e0f2c250c55f74
Shukhrat Khuseynov <58346267+shuhrat1998@users.noreply.github.com>
2019-12-09 05:44:58 +0300

    Description
    Embedding Cylinder code (without STL saving) as well.

--- UI_REV0.py
+++ UI_REV0.py
@@ -173,7 +173,29 @@ def Sph():
     
     
 def Cyl():
-    pass
+    
+    fig = plt.figure(figsize=(6,5), dpi=100)
+    canvas = FigureCanvasTkAgg(fig, window)
+    canvas.draw()
+
+    r=10; h=10; a=0; nt=100; nv =50;
+    theta = np.linspace(0, 2*np.pi, nt)
+    v = np.linspace(a, a+h, nv )
+    theta, v = np.meshgrid(theta, v)
+    x = r*np.cos(theta)
+    y = r*np.sin(theta)
+    z = v
+    rstride = 20
+    cstride = 10
+    
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(x, y, z, alpha=0.2, rstride=rstride, cstride=cstride)
+    ax.plot_surface(x, -y, z, alpha=0.2, rstride=rstride, cstride=cstride)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+    
+    canvas.get_tk_widget().grid(column=2, row=1)
 
 def Pyr():
     fig = plt.figure(figsize=(6,5), dpi=100)
