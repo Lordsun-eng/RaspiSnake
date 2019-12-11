4e7489802f9d45aea52714a4d85a299fee5544f1
Shukhrat Khuseynov <58346267+shuhrat1998@users.noreply.github.com>
2019-12-12 01:04:39 +0300

    Update UI_REV0.py
    GUI improvement & Adding "set parameters" feature to the plots & Adding feature of setting equal aspect for current figures.

--- UI_REV0.py
+++ UI_REV0.py
@@ -18,7 +18,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 
 def New():
     # cleaning the previous title
-    lb = Label(window, text=35*" ")
+    lb = Label(window, text=45*" ")
     lb.grid(column=2, row=1)
 
     fig = plt.figure(figsize=(6,5), dpi=100)
@@ -38,6 +38,7 @@ def New():
     scale = currfile.points.flatten(-1)
     ax.auto_scale_xyz(scale, scale, scale)
 
+    # naming the axes
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
@@ -59,7 +60,7 @@ def Opn():
     OPENF.save('1.stl', mode=stl.Mode.ASCII)  # save as ASCII
 
     # cleaning the previous title
-    lb = Label(window, text=35*" ")
+    lb = Label(window, text=45*" ")
     lb.grid(column=2, row=1)
     
     fig = plt.figure(figsize=(6,5), dpi=100)
@@ -130,19 +131,19 @@ def Save():
     
 def Cub():
     # cleaning the previous title
-    lb = Label(window, text=35*" ")
+    lb = Label(window, text=45*" ")
     lb.grid(column=2, row=1)
 
     fig = plt.figure(figsize=(6,5), dpi=100)
     canvas = FigureCanvasTkAgg(fig, window)
     canvas.draw()
     ax = fig.add_subplot(111, projection='3d')
-
+    
     # printing the according title
-    lb = Label(window, text="Rectangular prism")
+    lb = Label(window, text="Box (rectangular prism)")
     lb.grid(column=2, row=1)
 
-    cube_definition = [(0,0,0), (0,1,0), (2,0,0), (0,0,3)]
+    cube_definition = [(0,0,0), (0,w,0), (l,0,0), (0,0,h)]
     
     cube_definition_array = [
         np.array(list(item))
@@ -177,7 +178,7 @@ def Cub():
     faces.set_facecolor((0,0,1,0.1))
 
     ax.add_collection3d(faces)
-
+    
     # Plot the points themselves to force the scaling of the axes
     ax.scatter(points[:,0], points[:,1], points[:,2], s=0)
 
@@ -185,19 +186,68 @@ def Cub():
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
+
+    # setting axes equal (aspect)
+    scaling = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz']); ax.auto_scale_xyz(*[[np.min(scaling), np.max(scaling)]]*3)
     
     canvas.get_tk_widget().grid(column=2, row=2)
 
-    # adding Save button
+    # set parameters button with relevant functions & save button
+    GraphFrame = tk.Frame(window)
+    btnSet = Button(GraphFrame, text="Set the parameters", width=20, command=SetCub)
+    btnSet.grid(column=0, row=0, padx=5)
     global name
-    name = 'RectPrism.png' 
-    btnSave = Button(window, text="Save", width=10, command=Save)
-    btnSave.grid(column=2, row=3, pady=5)
+    name = 'Box.png' 
+    btnSave = Button(GraphFrame, text="Save", width=10, command=Save)
+    btnSave.grid(column=1, row=0, padx=5)
+    GraphFrame.grid(row=3, column=2, pady=5)
+def SetCub():
+    global setp
+    setp = tk.Tk()
+    setp.title("Set the Box")
+
+    lb = Label(setp, text="   The figure is Box.\nEnter its parameter(s):")
+    lb.grid(column=0, row=0, pady=10, padx=15)
+
+    global length, width, height
+    fr = tk.Frame(setp)
+    fr.grid(row=1, column=0, pady=20)
     
+    lb1 = Label(fr, text="Length")
+    lb1.grid(column=0, row=0, padx=7)
+    length = Entry(fr,width=5) 
+    length.grid(column=1, row=0, padx=8)
+    length.insert(0, str(l))
+
+    lb2 = Label(fr, text="Width")
+    lb2.grid(column=0, row=1, padx=7)
+    width = Entry(fr,width=5) 
+    width.grid(column=1, row=1, padx=8)
+    width.insert(0, str(w))
+
+    lb3 = Label(fr, text="Height")
+    lb3.grid(column=0, row=2, padx=7)
+    height = Entry(fr,width=5) 
+    height.grid(column=1, row=2, padx=8)
+    height.insert(0, str(h))
+
+    length.focus()
+    
+    btnOk = Button(fr, text="OK", width=4, command=OkCub)
+    btnOk.grid(column=1, row=3, padx=8, pady=15)
+
+    setp.geometry('150x200')
+def OkCub():
+    global l, w, h, setp
+    l = float(length.get())
+    w = float(width.get())
+    h = float(height.get())
+    Cub()
+    setp.destroy()
     
 def Sph():
     # cleaning the previous title
-    lb = Label(window, text=35*" ")
+    lb = Label(window, text=45*" ")
     lb.grid(column=2, row=1)
         
     fig = plt.figure(figsize=(6,5), dpi=100)            
@@ -223,7 +273,7 @@ def Sph():
     
     canvas.get_tk_widget().grid(column=2, row=2)
 
-    # set parameters function (have not added to other figures yet)
+    # set parameters button with relevant functions & save button
     GraphFrame = tk.Frame(window)
     btnSet = Button(GraphFrame, text="Set the parameters", width=20, command=SetSph)
     btnSet.grid(column=0, row=0, padx=5)
@@ -237,7 +287,7 @@ def SetSph():
     setp = tk.Tk()
     setp.title("Set the Sphere")
 
-    lb = Label(setp, text="The figure is Sphere.\nEnter its parameter(s):")
+    lb = Label(setp, text=" The figure is Sphere.\nEnter its parameter(s):")
     lb.grid(column=0, row=0, pady=10, padx=15)
 
     fr = tk.Frame(setp)
@@ -263,7 +313,7 @@ def OkSph():
     
 def Cyl():
     # cleaning the previous title
-    lb = Label(window, text=35*" ")
+    lb = Label(window, text=45*" ")
     lb.grid(column=2, row=1)
     
     fig = plt.figure(figsize=(6,5), dpi=100)
@@ -274,7 +324,10 @@ def Cyl():
     lb = Label(window, text="Cylinder")
     lb.grid(column=2, row=1)
 
-    r=10; h=10; a=0; nt=100; nv =50;
+    global r_cyl, h_cyl
+    r=r_cyl; h=h_cyl;
+    a=0; nt=100; nv =50;
+    
     theta = np.linspace(0, 2*np.pi, nt)
     v = np.linspace(a, a+h, nv )
     theta, v = np.meshgrid(theta, v)
@@ -292,18 +345,61 @@ def Cyl():
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
+
+    # setting axes equal (aspect)
+    scaling = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz']); ax.auto_scale_xyz(*[[np.min(scaling), np.max(scaling)]]*3)
     
     canvas.get_tk_widget().grid(column=2, row=2)
 
-    # adding Save button
+    # set parameters button with relevant functions & save button
+    GraphFrame = tk.Frame(window)
+    btnSet = Button(GraphFrame, text="Set the parameters", width=20, command=SetCyl)
+    btnSet.grid(column=0, row=0, padx=5)
     global name
     name = 'Cylinder.png' 
-    btnSave = Button(window, text="Save", width=10, command=Save)
-    btnSave.grid(column=2, row=3, pady=5)
+    btnSave = Button(GraphFrame, text="Save", width=10, command=Save)
+    btnSave.grid(column=1, row=0, padx=5)
+    GraphFrame.grid(row=3, column=2, pady=5)
+def SetCyl():
+    global setp
+    setp = tk.Tk()
+    setp.title("Set the Sphere")
 
+    lb = Label(setp, text="The figure is Cylinder.\nEnter its parameter(s):")
+    lb.grid(column=0, row=0, pady=10, padx=15)
+
+    global radius, height
+    fr = tk.Frame(setp)
+    fr.grid(row=1, column=0, pady=20) 
+
+    lb1 = Label(fr, text="Radius")
+    lb1.grid(column=0, row=0, padx=7)
+    radius = Entry(fr,width=5) 
+    radius.grid(column=1, row=0, padx=8)
+    radius.insert(0, str(r_cyl))
+
+    lb2 = Label(fr, text="Height")
+    lb2.grid(column=0, row=1, padx=7)
+    height = Entry(fr,width=5) 
+    height.grid(column=1, row=1, padx=8)
+    height.insert(0, str(h_cyl))
+
+    radius.focus()
+    
+    btnOk = Button(fr, text="OK", width=4, command=OkCyl)
+    btnOk.grid(column=1, row=2, padx=8, pady=15)
+
+    setp.geometry('150x170')
+def OkCyl():
+    global r_cyl, h_cyl, setp
+    r_cyl = float(radius.get())
+    h_cyl = float(height.get())
+    Cyl()
+    setp.destroy()
+    
 def Pyr():
     # cleaning the previous title
-    lb = Label(window, text=35*" ")
+    lb = Label(window, text=45*" ")
     lb.grid(column=2, row=1)
     
     fig = plt.figure(figsize=(6,5), dpi=100)
@@ -314,9 +410,10 @@ def Pyr():
     # printing the according title
     lb = Label(window, text="Pyramid")
     lb.grid(column=2, row=1)
-    
+
+    global b, h_pyr
     # vertices of a pyramid
-    v = np.array([[-1, -1, -1], [1, -1, -1], [1, 1, -1],  [-1, 1, -1], [0, 0, 1]])
+    v = np.array([[0, 0, 0], [b, 0, 0], [b, b, 0],  [0, b, 0], [b/2, b/2, h_pyr]])
     ax.scatter3D(v[:, 0], v[:, 1], v[:, 2])
 
     # generate list of sides' polygons of our pyramid
@@ -330,18 +427,61 @@ def Pyr():
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
+
+    # setting axes equal (aspect)
+    scaling = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz']); ax.auto_scale_xyz(*[[np.min(scaling), np.max(scaling)]]*3)
     
     canvas.get_tk_widget().grid(column=2, row=2)
 
-    # adding Save button
+    # set parameters button with relevant functions & save button
+    GraphFrame = tk.Frame(window)
+    btnSet = Button(GraphFrame, text="Set the parameters", width=20, command=SetPyr)
+    btnSet.grid(column=0, row=0, padx=5)
     global name
     name = 'Pyramid.png' 
-    btnSave = Button(window, text="Save", width=10, command=Save)
-    btnSave.grid(column=2, row=3, pady=5)
+    btnSave = Button(GraphFrame, text="Save", width=10, command=Save)
+    btnSave.grid(column=1, row=0, padx=5)
+    GraphFrame.grid(row=3, column=2, pady=5)
+def SetPyr():
+    global setp
+    setp = tk.Tk()
+    setp.title("Set the Pyramid")
+
+    lb = Label(setp, text="The figure is Pyramid.\nEnter its parameter(s):")
+    lb.grid(column=0, row=0, pady=10, padx=15)
+
+    global base, height
+    fr = tk.Frame(setp)
+    fr.grid(row=1, column=0, pady=20) 
+
+    lb1 = Label(fr, text="Base length")
+    lb1.grid(column=0, row=0, padx=7)
+    base = Entry(fr,width=5) 
+    base.grid(column=1, row=0, padx=8)
+    base.insert(0, str(b))
+
+    lb2 = Label(fr, text="Height")
+    lb2.grid(column=0, row=1, padx=7)
+    height = Entry(fr,width=5) 
+    height.grid(column=1, row=1, padx=8)
+    height.insert(0, str(h_pyr))
+
+    base.focus()
+    
+    btnOk = Button(fr, text="OK", width=4, command=OkPyr)
+    btnOk.grid(column=1, row=2, padx=8, pady=15)
+
+    setp.geometry('150x170')
+def OkPyr():
+    global b, h_pyr, setp
+    b = float(base.get())
+    h_pyr = float(height.get())
+    Pyr()
+    setp.destroy()
 
 def Paral():
     # cleaning the previous title
-    lb = Label(window, text=35*" ")
+    lb = Label(window, text=45*" ")
     lb.grid(column=2, row=1)
 
     fig = plt.figure(figsize=(6,5), dpi=100)
@@ -353,14 +493,15 @@ def Paral():
     lb = Label(window, text="Parallelepiped")
     lb.grid(column=2, row=1)
 
-    points = np.array([[-1, -1, -1],
-                  [1, -1, -1 ],
-                  [1, 1, -1],
-                  [-1, 1, -1],
-                  [-1, -1, 1],
-                  [1, -1, 1 ],
-                  [1, 1, 1],
-                  [-1, 1, 1]])
+    global s
+    points = np.array([[0, 0, 0],
+                  [s, 0, 0 ],
+                  [s, s, 0],
+                  [0, s, 0],
+                  [0, 0, s],
+                  [s, 0, s],
+                  [s, s, s],
+                  [0, s, s]])
 
     P = [[2.06498904e-01 , -6.30755443e-07 ,  1.07477548e-03],
      [1.61535574e-06 ,  1.18897198e-01 ,  7.85307721e-06],
@@ -393,13 +534,53 @@ def Paral():
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
 
+    # setting axes equal (aspect)
+    scaling = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz']); ax.auto_scale_xyz(*[[np.min(scaling), np.max(scaling)]]*3)
+    
     canvas.get_tk_widget().grid(column=2, row=2)
 
-    # adding Save button
+    # set parameters button with relevant functions & save button
+    GraphFrame = tk.Frame(window)
+    btnSet = Button(GraphFrame, text="Set the parameters", width=20, command=SetParal)
+    btnSet.grid(column=0, row=0, padx=5)
     global name
     name = 'Parallelepiped.png' 
-    btnSave = Button(window, text="Save", width=10, command=Save)
-    btnSave.grid(column=2, row=3, pady=5)
+    btnSave = Button(GraphFrame, text="Save", width=10, command=Save)
+    btnSave.grid(column=1, row=0, padx=5)
+    GraphFrame.grid(row=3, column=2, pady=5)
+def SetParal():
+    global setp
+    setp = tk.Tk()
+    setp.title("Set the Parallelepiped")
+
+    lb = Label(setp, text="The figure is Parallelepiped.\nEnter its parameter(s):")
+    lb.grid(column=0, row=0, pady=10, padx=15)
+
+    fr = tk.Frame(setp)
+    fr.grid(row=1, column=0, pady=20) 
+    lb1 = Label(fr, text="One side")
+    lb1.grid(column=0, row=0, padx=7)
+    
+    global side
+    side = Entry(fr,width=5) 
+    side.grid(column=1, row=0, padx=8)
+    side.insert(0, str(s))
+
+    lb2 = Label(fr, text="Two other sides")
+    lb2.grid(column=0, row=1, padx=7)
+    lb3 = Label(fr, text="2 * side1")
+    lb3.grid(column=1, row=1, padx=7)
+    side.focus()
+    
+    btnOk = Button(fr, text="OK", width=4, command=OkParal)
+    btnOk.grid(column=1, row=2, padx=8, pady=15)
+
+    setp.geometry('170x170')
+def OkParal():
+    global s, setp
+    s = float(side.get())
+    Paral()
+    setp.destroy()
 
 """
 # Using an existing stl file:
@@ -447,13 +628,22 @@ btnOpn.grid(column=1, row=0, padx=3)
 FileFrame.grid(row=1, column=0, sticky="nsew", padx=10, pady=5)
 
 # default parameters
-r = 1
+r = 1.0 # radius of the sphere
+l = 1.5 # length of the box
+w = 1.0 # width of the box
+h = 2.0 # height of the box
+
+r_cyl = 10.0 # radius of the cylinder
+h_cyl = 20.0 # height of the cylinder
+b = 10.0 # base length of the pyramid
+h_pyr = 10.0 # height of the pyramid
+s = 2 # one side of the parallelepiped
 
 # Figure plotting buttons
 PlotFrame = tk.Frame(window)
 lb = Label(PlotFrame, text="Choose a figure:\n")
 lb.grid(column=0, row=0, padx=0, pady=0)
-btnCub = Button(PlotFrame, text="Rectangular prism", width=16, command=Cub)
+btnCub = Button(PlotFrame, text="Box", width=16, command=Cub)
 btnCub.grid(column=0, row=1, pady=3)
 btnSph = Button(PlotFrame, text="Sphere", width=16, command=Sph)
 btnSph.grid(column=0, row=2, pady=2)
