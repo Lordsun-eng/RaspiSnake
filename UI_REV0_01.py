4cb91d3452df3b901264d4684e0759814189e275
Lordsun-eng <55994739+Lordsun-eng@users.noreply.github.com>
2019-12-20 13:43:15 +0300

    First release0.01

--- UI_REV0_01.py
+++ UI_REV0_01.py
@@ -86,12 +86,11 @@ def New():
     data = np.zeros(100, dtype=mesh.Mesh.dtype)
     New = mesh.Mesh(data, remove_empty_areas=False)
     New.save('RaspS.stl', mode=stl.Mode.ASCII)
-    loc_main_body = mesh.Mesh.from_file('RaspS.stl')
-    ax.add_collection3d(mplot3d.art3d.Poly3DCollection(loc_main_body.vectors))
-    scale = loc_main_body.points.flatten('F')
+    new_main_body = mesh.Mesh.from_file('RaspS.stl')
+    ax.add_collection3d(mplot3d.art3d.Poly3DCollection(new_main_body.vectors))
+    scale = new_main_body.points.flatten('F')
     ax.auto_scale_xyz(scale, scale, scale)
 
-    canvas.get_tk_widget().grid(row=0, column=1)
     canvas.draw()
     
 def Opn():
@@ -114,11 +113,15 @@ def Opn():
     scale = loc_main_body.points.flatten('F')
     ax.auto_scale_xyz(scale, scale, scale)
 
-    canvas.get_tk_widget().grid(row=0, column=1)
     canvas.draw()
     
-def Save():
-    pass
+def Sav():
+    window.filename=tk.filedialog.asksaveasfile(initialdir = "/",title = "Select file",filetypes = [("STL files","*.stl")])
+    fileout =window.filename+".stl"
+    loc_main_body = mesh.Mesh.from_file('RaspS.stl')
+    loc_main_body.save(fileout, mode=stl.Mode.ASCII)  # save as ASCII
+    
+    name.close
 
 def Ext():
     quit()
@@ -173,9 +176,8 @@ def Cub():
         scale = loc_main_body.points.flatten('C')
         ax.auto_scale_xyz(scale, scale, scale)
 
-        canvas.get_tk_widget().grid(row=0, column=1)
         canvas.draw()
-        main_body=loc_main_body
+
         Cubimp.grab_release()
         Cubimp.destroy()
         
@@ -226,39 +228,28 @@ def Cub():
     btnOK.grid(column=0, row=1)
     btnCLC.grid(column=1, row=1)
     CFrame.grid(column=0, row=1)
-    
-    
-        
-    
-
-
-    
+      
 def Sph():
     def Sphcal():
+        Rad=float(R.get())
+        XYZ=[float(xc.get()),float(yc.get()),float(zc.get())]
         n=20
-        Rad=20
-        dx=dz=n/Rad
-
-
-        xc,yc,zc=0,0,0
+        faces=[]
+        nf=0        
         Nod=[]
-        xmax=xc+Rad
-        ymax=yc+Rad
-        zmax=zc+Rad
-        x=xc-Rad
-        y=yc-Rad
-        z=zc-Rad
+        dx=dz=Rad/n
+        x=XYZ[0]-Rad
+        z=XYZ[2]-Rad
         for Z in range(n):
             z=z+dz
-            dx=math.sqrt(Rad**2-z**2)/n
-            x=xc-math.sqrt(Rad**2-z**2)
+            dx=math.sqrt((Rad**2-(z-XYZ[2])**2))/n
+            x=XYZ[0]-math.sqrt(abs(Rad**2-(z-XYZ[2])**2))
             for X in range(n):
-                y=math.sqrt(abs(Rad**2-z**2-x**2))
+                y=math.sqrt(abs(Rad**2-(z-XYZ[2])**2-(x-XYZ[0])**2))
                 Nod.append([x,y,z])
                 x=x+dx
-        Nod.append([0,0,zc-Rad])
-        faces=[]
-        nf=0
+        Nod.append([0,0,XYZ[2]-Rad])
+
         for Z in range(n-1):
             
             for X in range(n):
@@ -329,11 +320,11 @@ def Sph():
         scale = loc_main_body.points.flatten('C')
         ax.auto_scale_xyz(scale, scale, scale)
     
-        canvas.get_tk_widget().grid(row=0, column=1)
         canvas.draw()
         main_body=loc_main_body
         Sphimp.grab_release()
         Sphimp.destroy()
+
     def SphCLC():
         Sphimp.grab_release()
         Sphimp.destroy()
@@ -347,8 +338,8 @@ def Sph():
     tk.Label(Spframe, text="X").grid(row=1)
     tk.Label(Spframe, text="Y").grid(row=2)
     tk.Label(Spframe, text="Z").grid(row=3)
-    Rad = tk.Entry(Spframe)
-    Rad.insert(tk.END, "1")
+    R = tk.Entry(Spframe)
+    R.insert(tk.END, "1")
     
     xc = tk.Entry(Spframe)
     xc.insert(tk.END, "0")
@@ -359,10 +350,10 @@ def Sph():
     zc = tk.Entry(Spframe)
     zc.insert(tk.END, "0")
 
-    Rad.grid(row=0, column=1)
-    xc.grid(row=3, column=1)
-    yc.grid(row=4, column=1)
-    zc.grid(row=5, column=1)
+    R.grid(row=0, column=1)
+    xc.grid(row=1, column=1)
+    yc.grid(row=2, column=1)
+    zc.grid(row=3, column=1)
     Spframe.grid(row=0, column=0)
             
     CFrame=tk.Frame(Sphimp)
@@ -374,8 +365,135 @@ def Sph():
 
         
 def Con():
-    pass
+    def Concal():
+        Rad=float(R.get())
+        h=float(H.get())
 
+        XYZ=[float(xc.get()),float(yc.get()),float(zc.get())]
+        n=20
+        faces=[]
+        nf=0        
+        Nod=[]
+        dx=Rad/n
+        x=XYZ[0]-Rad
+        z=XYZ[2]
+        Nod.append([0,0,XYZ[2]])
+        for X in range(n+1):
+            y=math.sqrt(Rad**2-(x-XYZ[0])**2)
+            Nod.append([x,y,z])
+            x=x+dx
+        Nod.append([0,0,XYZ[2]+h])
+        
+        faces=[]
+        nf=0
+        for X in range(1,n+1):
+            faces.append([X,X+1,-1])
+            faces.append([X,X+1,0])
+            
+        faces=np.array(faces)
+        Nod=np.array(Nod)
+        con = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+        for i, f in enumerate(faces):
+            for j in range(3):
+                con.vectors[i][j] = Nod[f[j],:]
+        CON1 = mesh.Mesh(con.data.copy())
+        CON2 = mesh.Mesh(con.data.copy())
+        CON3 = mesh.Mesh(con.data.copy())
+        CON4 = mesh.Mesh(con.data.copy())
+        CON2.rotate([0, 0, 1], math.radians(90))
+        CON3.rotate([0, 0, 1], math.radians(180))
+        CON4.rotate([0, 0, 1], math.radians(270))
+        
+        con = mesh.Mesh(np.concatenate([
+        CON1.data.copy(),
+        CON2.data.copy(),
+        CON3.data.copy(),
+        CON4.data.copy(),
+        ]))
+    
+        openf = copy_obj(con, (w1, l1, h1), 1, 1, 1)
+    
+        minx, maxx, miny, maxy, minz, maxz = find_mins_maxs(con)
+        w2 = maxx - minx
+        l2 = maxy - miny
+        h2 = maxz - minz
+        translate(con, w1, w1 / 10., 3, 'x')
+        main_body = mesh.Mesh.from_file('RaspS.stl')
+        combined = mesh.Mesh(np.concatenate([main_body.data, con.data]))
+        combined.save('RaspS.stl', mode=stl.Mode.ASCII)  # save as ASCII
+        loc_main_body = mesh.Mesh.from_file('RaspS.stl')
+
+        ax.add_collection3d(mplot3d.art3d.Poly3DCollection(loc_main_body.vectors))
+        scale = loc_main_body.points.flatten('C')
+        ax.auto_scale_xyz(scale, scale, scale)
+    
+        canvas.get_tk_widget().grid(row=0, column=1)
+        canvas.draw()
+        main_body=loc_main_body
+        Conimp.grab_release()
+        Conimp.destroy()
+    def ConCLC():
+        Conimp.grab_release()
+        Conimp.destroy()
+        
+    Conimp=tk.Toplevel() 
+    Conimp.grab_set()
+    Conimp.overrideredirect(True)
+    
+    Coframe=tk.Frame(Conimp)
+    tk.Label(Coframe, text="Radius").grid(row=0)
+    tk.Label(Coframe, text="Height").grid(row=1)
+    tk.Label(Coframe, text="X").grid(row=2)
+    tk.Label(Coframe, text="Y").grid(row=3)
+    tk.Label(Coframe, text="Z").grid(row=4)
+    R = tk.Entry(Coframe)
+    R.insert(tk.END, "1")
+    H = tk.Entry(Coframe)
+    H.insert(tk.END, "1")
+    
+    xc = tk.Entry(Coframe)
+    xc.insert(tk.END, "0")
+
+    yc = tk.Entry(Coframe)
+    yc.insert(tk.END, "0")
+
+    zc = tk.Entry(Coframe)
+    zc.insert(tk.END, "0")
+
+    R.grid(row=0, column=1)
+    H.grid(row=1, column=1)
+    xc.grid(row=2, column=1)
+    yc.grid(row=3, column=1)
+    zc.grid(row=4, column=1)
+    Coframe.grid(row=0, column=0)
+            
+    CFrame=tk.Frame(Conimp)
+    btnOK = tk.Button(CFrame, text = "OK" , width=10, height=1,command=Concal)
+    btnCLC= tk.Button(CFrame, text = "Cancel" , width=10, height=1,command=ConCLC)
+    btnOK.grid(column=0, row=1)
+    btnCLC.grid(column=1, row=1)
+    CFrame.grid(column=0, row=1)
+def MshP():
+    Mesh = mesh.Mesh.from_file('RaspS.stl')
+
+    volume, cog, inertia = Mesh.get_mass_properties()
+    PRP=tk.Toplevel() 
+    PRP.grab_set()
+    PRP.overrideredirect(True)
+    
+    Coframe=tk.Frame(PRP)
+    tk.Label(Coframe, text="Volume                                  = {0}".format(volume)).grid(row=0)
+    tk.Label(Coframe, text="Position of the center of gravity (COG) = {0}".format(cog)).grid(row=1)
+    tk.Label(Coframe, text="Inertia matrix at expressed at the COG  = {0}").grid(row=2)
+    tk.Label(Coframe, text="                                          {0}").grid(row=3)
+    tk.Label(Coframe, text="                                          {0}").grid(row=4)
+    Coframe.grid(row=0, column=0)
+    def PRPOK():
+        PRP.grab_release()
+        PRP.destroy()        
+    btnOK = tk.Button(PRP, text = "OK" , width=10, height=1,command=PRPOK)
+    btnOK.grid(column=0, row=1)
+ 
 
 # Splash screen (creating a splash screen, 80% of display screen size, centered, displaying a GIF image with needed info, disappearing after 5 seconds)
 root = tk.Tk()
@@ -391,10 +509,10 @@ canvas = tk.Canvas(root, height=height*0.6, width=width*0.6, bg="white")
 canvas.create_image(width*0.6/2, height*0.6/2, image=image)
 
 
-# Showing the splash screen for 5000 milliseconds then destroying
-root.after(1, root.destroy)
+# Showing the splash screen for 2000 milliseconds then destroying
+root.after(2000, root.destroy)
 root.mainloop()
-print ("RaspiSnakes")
+print ("RS Design is Runing")
 
 # Starting the main window 
 window = tk.Tk()
@@ -415,10 +533,14 @@ imgOpn = tk.PhotoImage(file="opn.png")
 btnOpn = tk.Button(FileFrame, image = imgOpn, width=20, height=20, command=Opn)
 btnOpn.image = imgOpn
 btnOpn.grid(column=0, row=1, padx=3)
+imgSav = tk.PhotoImage(file="Sav.png")
+btnSav = tk.Button(FileFrame, image = imgSav, width=20, height=20, command=Sav)
+btnSav.image = imgSav
+btnSav.grid(column=0, row=2, padx=3)
 imgExt = tk.PhotoImage(file="ext.png")
 btnExt = tk.Button(FileFrame, image = imgExt, width=20, height=20, command=Ext)
 btnExt.image = imgExt
-btnExt.grid(column=0, row=2, padx=3)
+btnExt.grid(column=0, row=3, padx=3)
 FileFrame.grid(row=0, column=0, sticky="nsew", padx=10)
         
 # Create toolbox
@@ -440,6 +562,16 @@ btnCon.image = imgCon
 btnCon.grid(column=0, row=3)
 
 CrtFrame.grid(row=0, column=2, sticky="nsew", padx=10)
+
+#STL Properties
+PPFrame = tk.Frame(window)
+lb = Label(PPFrame)
+lb.grid(column=0, row=0)
+imgPRP = tk.PhotoImage(file="PRP.png")
+btnPRP = tk.Button(PPFrame, image =imgPRP, width=20, height=20, command=MshP)
+btnPRP.image = imgPRP
+btnPRP.grid(column=0, row=0)
+PPFrame.grid(row=2, column=1, sticky="nsew", padx=10)
 
 
 window.title("Rasp Snakes Software VER 0.01")
